da67078ad564a7bef48e711904345e054628f2e2
techrabbit58 <dzb@pruefbit.de>
2019-09-01 18:26:21 +0200

    check in

---
+++ src/main/java/com/example/babysteps/HelloWorldMessage.java
@@ -0,0 +1,15 @@
+package com.example.babysteps;
+
+import javax.swing.*;
+
+public class HelloWorldMessage {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(
+                null,
+                "Hello, world!",
+                "Message Box",
+                JOptionPane.INFORMATION_MESSAGE);
+    }
+
+}
